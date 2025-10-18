4cb72c570699ce97013f119c921960fdb187aabf
V-Zholobchuk <vasyl.zholobchuk.23@pnu.edu.ua>
2025-10-18 19:01:46 +0300

    Feature: Add users blueprint and templates

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
-from flask import Flask, render_template, url_for
-
-app = Flask(__name__)
-
-@app.route('/')
-def resume():
-    return render_template('resume.html', title='Резюме')
-
-@app.route('/contacts')
-def contacts():
-    return render_template('contacts.html', title='Контакти')
+from app import app 
 
 if __name__ == '__main__':
     app.run(debug=True)
