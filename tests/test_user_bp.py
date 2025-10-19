022bcf43b5c4d6491ce89862b1986281078fbe08
V-Zholobchuk <vasyl.zholobchuk.23@pnu.edu.ua>
2025-10-19 16:43:48 +0300

    add test_product_bp.py

--- tests/test_user_bp.py
+++ tests/test_user_bp.py
@@ -13,19 +13,19 @@ class FlaskAppTestCase(unittest.TestCase):
 
     def test_greetings_page(self):
         """Тест маршруту /hi/<name>."""
-        response = self.client.get("/hi/John?age=30") [cite: 186]
+        response = self.client.get("/hi/John?age=30") 
 
-        self.assertEqual(response.status_code, 200) [cite: 187]
-        self.assertIn(b"JOHN", response.data) [cite: 188]
-        self.assertIn(b"30", response.data) [cite: 189]
+        self.assertEqual(response.status_code, 200) 
+        self.assertIn(b"JOHN", response.data) 
+        self.assertIn(b"30", response.data) 
 
     def test_admin_page(self):
         """Тест маршруту /admin, який перенаправляє."""
-        response = self.client.get("/admin", follow_redirects=True) [cite: 191]
+        response = self.client.get("/admin", follow_redirects=True) 
 
-        self.assertEqual(response.status_code, 200) [cite: 192]
-        self.assertIn(b"ADMINISTRATOR", response.data) [cite: 193]
-        self.assertIn(b"45", response.data) [cite: 193]
+        self.assertEqual(response.status_code, 200) 
+        self.assertIn(b"ADMINISTRATOR", response.data) 
+        self.assertIn(b"45", response.data) 
 
 if __name__ == "__main__":
     unittest.main()
